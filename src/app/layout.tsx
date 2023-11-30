eb2c84c2ae8e75973e8909b059bb4c9fc7d14308
frankkk96 <langtang1996@gmail.com>
2023-11-30 17:09:58 +0800

    fix auth bug

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <ClerkProvider afterSignInUrl='/' afterSignUpUrl='/' >
+    <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>{children}</body>
       </html>
